b490984dd503abe29c110793070d6d3369fd70d7
Sébastien PHILIPPE <sebastien.philippe@hoppen.care>
2023-11-23 15:50:52 +0100

    add camera and foders capacities

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import  {useCallback, useState} from 'react';
 
 import {Button} from "@mui/material"
+import {Camera} from "@capacitor/camera"
 import { Geolocation } from '@capacitor/geolocation';
+import { Keyboard } from '@capacitor/keyboard';
 
 export default function GeolocationPage() {
 
   const [loc, setLoc] = useState(null);
+  const [photoFolder,setPhotoFolder]=useState([])
 
   const getCurrentPosition = useCallback(async () => {
     const coordinates = await Geolocation.getCurrentPosition();
     setLoc(coordinates);
   }, []);
 
+  const getImages = useCallback(async()=>{
+    const photos = await Camera.pickImages()
+    setPhotoFolder(photos)
+    console.log(photos)
+  },[])
+
   return (
-    <div className='grid place-content-center'>
+    <div className='grid place-content-center gap-5'>
       <h1>Geolocation</h1>
       <p>Your location is:</p>
       <p>Latitude: {loc?.coords.latitude}</p>
@@ -22,9 +31,14 @@ export default function GeolocationPage() {
       <Button color='secondary' onClick={getCurrentPosition}>
         Get Current Location
       </Button>
-      <Button variant='contained' color='primary' onClick={getCurrentPosition}>
-        2 ème bouton
+      <Button variant='contained' color='primary' onClick={getImages}>
+        Open file system
       </Button>
+      <Button variant='contained' color='secondary' onClick={()=>Keyboard.show()}>
+        Open keyboard
+      </Button>
+      {photoFolder?.photos?.map(picture=><img key={picture} width={'500'} src={picture.webPath} onClick={()=>getImages()}/>)}
       </div>
+      
   );
 }
